24b3b9bef8e7dbe1ea5c1701919299dbee88425c
littelytle <129564810+littelytle@users.noreply.github.com>
2026-02-06 00:39:38 -0700

    Update AddStudentModal.tsx

--- components/AddStudentModal.tsx
+++ components/AddStudentModal.tsx
@@ -1,6 +1,5 @@
-
 import React, { useState, useEffect } from 'react';
-import { Grade, Student, Subject } from './types'; // Fixed: ./ instead of ../
+import { Grade, Student, Subject } from './types'; // Changed from ../
 import { Button } from './Button';
 
 interface AddStudentModalProps {
